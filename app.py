d09de9f5b9842fa17235f8096ce79a3cf7716ebd
keevinzha <keevinzha@163.com>
2025-05-22 16:02:52 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, url_for, render_template, request
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+
+            return render_template('login.html', error=error)
